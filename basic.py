5a897a3f7bdaafa5d19da751b26c22fc68952a4a
Kainat <kainatawaan33@gmail.com>
2026-02-25 07:08:05 +0500

    these are function and veriable of python

---
+++ basic.py
@@ -0,0 +1,19 @@
+## data types 
+#Numeric
+# integer: 23424, -3,0,-234
+# float:234.098, -23.0, -1.0
+#text
+# string: "we are learning datatypes", 'abc garden town', '''text''',"""text"""
+#boolean: True,False
+
+##collections
+# list
+cloud_class_students = ["ali", "hamza","abc","aaa"]
+
+# tuple
+cloud_class = ("ali", "hamza","abc","aaa")
+
+# dictionary
+student_data = {'name':"ali",'age':17,"address":"abc street multan"}
+
+set:{12,14,12}
