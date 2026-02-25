4fd9caeecf14556770507e80bf4a751cbbf7d5d8
Kainat Sikandar <kainatawaan33@gmail.com>
2026-02-25 15:20:55 +0500

    conditional statments of python

---
+++ conditional_statment.py
@@ -0,0 +1,41 @@
+#conditional statment
+#condation
+# conditional operators
+# > = greater then
+# < = less then
+# == is equal
+# != not equal
+# >= greater then or equal
+# <= less then or equal
+
+print(12 > 5)
+print(4<3)
+print(4 == 4)
+print(2!=2)
+print(15 >= 5)
+
+value = 34
+val = 20
+print(value <= val)
+
+
+# if single
+# if-else two block
+#if-elif-else multiblock
+ 
+
+# Write a program to show mark and 
+# say congragulation if marks are above 60.
+marks = float(input("enter your masks:"))
+
+
+if marks > 60:
+  print("congragulation you're passed", marks)
+  percentage = marks/100*100
+  print("your percentage is:" , percentage, "%" ) 
+
+ 
+
+
+     
+
---
+++ program.py
@@ -0,0 +1,8 @@
+# write a program that get two integer values from the User
+# and multiply the output 
+
+
+user1 = int(input("enter first number:"))
+user2 = int(input("enter second number:"))
+Multiply = user1 * user2
+print("the result of multiplication is: multiply")
