a7ed1a5ddb3d1d0a136d598b3f3170b431b5bab4
Kainat Sikandar <kainatawaan33@gmail.com>
2026-02-28 15:51:11 +0500

    these are programs

--- if else.py
+++ if else.py
@@ -21,3 +21,29 @@ if number >0:
 else:
     print("The number",number ,"is negitive")   
 
+#check whether  age is able to vote or not
+
+age = int(input("enter your age"))
+
+if age >18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")   
+
+# check which number is greater
+
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+if num1 > num2:
+    print("First number is greater")
+else:
+    print("Second number is greater")
+
+#check level of water is high or low
+
+level = int(input("enter level of water"))
+
+if >50:
+    print("level of water is high")
+else:
+    print("level of water is low")    
